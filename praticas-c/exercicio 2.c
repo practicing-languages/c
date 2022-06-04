71f16450bf2008b650bb0cd079917f88aeebec13
CoelhoRafael <rafael.coelho@bandtec.com.br>
2022-06-04 20:20:36 -0300

    FEAT: first commit

---
+++ praticas-c/exercicio 2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main (){
+int a = 8;
+printf("o valor da variavel a e igual: %d ", a*5);
+//printf("%d", a);
+printf("\n\n\n\nolaa");
+system ("pause");
+}
